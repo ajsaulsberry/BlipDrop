a65d29525eb29d156c513d44b101275972688eda
ajsaulsberry <ajsaulsberry@outlook.com>
2017-08-13 11:36:13 -0400

    Add project files.

---
+++ BlipDrop/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace BlipDrop
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
